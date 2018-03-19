2307f9626fa667e9fdc780bb729f8c5d50c473e2
doriholm <doriholm@gmail.com>
2018-03-20 00:36:56 +0100

    Initial commit

---
+++ Exercise4/BarDetailsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Exercise4
+{
+    public partial class BarDetailsPage : ContentPage
+    {
+        public BarDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        void CheckInPage(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Exercise4Page());
+
+        }
+    }
+}
---
+++ Exercise4/Exercise4Page.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Exercise4
+{
+    public partial class Exercise4Page : ContentPage
+    {
+        public Exercise4Page()
+        {
+            InitializeComponent();
+        }
+
+
+        void HomePage(object sender, System.EventArgs e)
+        {
+            Navigation.PopAsync();
+
+
+        }
+    }
+}
